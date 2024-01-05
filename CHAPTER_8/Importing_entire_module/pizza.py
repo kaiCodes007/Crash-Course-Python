f746a97162604371ea9fe09eee108215f08ef2f0
kaiCodes007 <key7core@gmail.com>
2024-01-06 00:22:12 +0545

    Importing modules in python

--- CHAPTER_8/Importing_entire_module/pizza.py
+++
@@ -1,7 +0,0 @@
-#Creating pizza module
-
-def make_pizza(size,*toppings):
-	print(f"\nMaking a {size}-inch pizza with following requested toppings: ")
-	for topping in toppings:
-		print(f"- {topping}")
-
