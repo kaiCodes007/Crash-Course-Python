0fd64d17af5f49a5613e653638daa53eb708bdc8
kaiCodes007 <key7core@gmail.com>
2024-01-06 10:27:27 +0545

    Removed .py file

--- CHAPTER_8/Importing_Modules/Excercise/Excercise.py
+++
@@ -1,9 +0,0 @@
-def printing_designs(unprinted, completed):
-	while unprinted:
-		current = unprinted.pop()
-		print(f"Printing model: {current.title()}")
-		completed.append(current)
-
-unprinted_models = ['phone case', 'robot pendant', 'dodecahedron']
-printed_designs = []
-printing_designs(unprinted_models, printed_designs)
